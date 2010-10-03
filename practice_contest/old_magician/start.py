6eb719231353dbdd4463f3ca581c195638677773
Marton Elek <elek@miro.localdomain>
2010-10-03 23:08:02 +0200

    old magician is solved

---
+++ practice_contest/old_magician/start.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+def solve():   
+   w,b = raw_input().split()
+   if int(b) % 2 == 0:
+      print "WHITE"
+   else:
+      print "BLACK"
+
+   
+def start():
+   for lin in range(input()):
+      print "Case #%s:"%(lin+1),
+      solve()
+
+if __name__ == '__main__':
+   start()
